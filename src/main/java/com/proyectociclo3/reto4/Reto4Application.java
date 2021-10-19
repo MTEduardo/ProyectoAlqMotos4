4f82d0878fe3c740606d672d47d704ec139e2972
EduardoGamez <eduardo.gamez.mt@correo.usa.edu.co>
2021-10-19 08:52:01 -0500

    Se modifica el main de Reto4Application a main2

--- src/main/java/com/proyectociclo3/reto4/Reto4Application.java
+++ src/main/java/com/proyectociclo3/reto4/Reto4Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Reto4Application {
 
-	public static void main(String[] args) {
+	public static void main2(String[] args) {
 		SpringApplication.run(Reto4Application.class, args);
 	}
 
